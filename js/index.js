b39d90d601d04a9ca9b7bda0f288aba787c8f277
Shiank <shiank333@gmail.com>
2023-01-30 21:55:10 -0500

    HW-19-3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-$(document).on('click', 'a[href^="#"]', function(event) {
-    event.perventDefault();
+/*$(document).on('click', 'a[href^="#"]', function(event) {
+    if (target.is $(".inline-block")){
+        event.preventDefault(); 
+    }
+    var hash = this.hash;
+
 
     $('html', body).animate({
         scrollTop: $($.attr(this,'href')).offset().top
-    }, 5000);
+    }, 1000);
+});*/
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+   
+    event.preventDefault();
+
+    $('html', body).animate({
+        scrollTop: $($.attr(this,'href')).offset().top
+    }, 1000);
+
 });
